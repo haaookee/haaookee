babfabba16bf3b41dd4f0dd9ca1247f9f01a7983
haaookee <774398469@qq.com>
2020-11-13 02:22:54 +0800

    example使用示例

--- server/src/main/java/com/course/server/domain/Test.java
+++ server/src/main/java/com/course/server/domain/Test.java
@@ -1,8 +1,9 @@
 package com.course.server.domain;
 
 public class Test {
-    private  String id;
-    private  String name;
+    private String id;
+
+    private String name;
 
     public String getId() {
         return id;
@@ -18,5 +19,17 @@ public class Test {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append("]");
+        return sb.toString();
     }
 }
