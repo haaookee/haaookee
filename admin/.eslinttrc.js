63c14e85b541d251e92cb3840ee29450b46e56bb
haaookee <774398469@qq.com>
2020-11-17 09:40:42 +0800

    增加login页面 配置路由

--- admin/.eslinttrc.js
+++ admin/.eslinttrc.js
@@ -10,11 +10,10 @@ module.exports = {
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-undef': 'off',
+
         'vue/no-unused-vars': 'off',
         'vue/require-v-for-key': 'off',
-        'no-unused-vars': 'off',
-        'vue/no-unused-components': 'off'
+
     },
     parserOptions: {
         parser: 'babel-eslint'
