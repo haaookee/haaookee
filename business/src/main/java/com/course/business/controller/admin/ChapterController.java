234f7c4f68872cf271f17615bf4957fd2c072e3b
haaookee <774398469@qq.com>
2020-11-17 15:32:26 +0800

    增加了chapterDto

--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -1,6 +1,7 @@
-package com.course.business.controller;
+package com.course.business.controller.admin;
 
 import com.course.server.domain.Chapter;
+import com.course.server.dto.ChapterDto;
 import com.course.server.servic.ChapterService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +11,14 @@ import java.util.List;
 
 
 @RestController
+@RequestMapping("/admin")
 public class ChapterController {
 
     @Resource
     private ChapterService chapterService;
 
     @RequestMapping("/chapter")
-    public List<Chapter> chapter(){
+    public List<ChapterDto> chapter(){
         return chapterService.list();
     }
 }
--- doc/db/all.sql
+++ doc/db/all.sql
@@ -6,6 +6,8 @@ create table `chapter`(
     primary key (`id`)
 ) engine=innodb default  charset=utf8 comment='大章';
 
+insert into `chapter`(id,course_id,name)values('00000000','00000000','测试大章');
+insert into `chapter`(id,course_id,name)values('00000001','00000000','测试大章2');
 
 
 drop table  if exists `test`;
