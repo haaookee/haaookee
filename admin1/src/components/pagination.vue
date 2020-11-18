19fd3cd49409bc61e7fef22303e4cece0beb326f
haaookee <774398469@qq.com>
2020-11-18 14:23:01 +0800

    增加新增大章功能 eslint关闭no-unused-vars

---
+++ admin1/src/components/pagination.vue
@@ -0,0 +1,126 @@
+<template>
+<div class="pagination" role="group" aria-label="分页">
+  <button type="button" class="btn btn-default btn-white btn-round"
+      v-bind:disabled="page === 1"
+     v-on:click="selectPage(1)">
+    1
+  </button>
+  <button type="button" class="btn btn-default btn-white btn-round"
+   v-bind:disabled="page === 1"
+    v-on:click="selectPage(page - 1)">
+    上一页
+  </button>
+<button v-for="p in pages" v-bind:id="'page-'+p"
+  type="button" class="btn btn-default btn-white btn-round"
+ v-bind:class="{'btn-primary.active':page == p}"
+ v-on:click="selectPage(p)">
+  {{p}}
+</button>
+
+  <button type="button" class="btn btn-default btn-white btn-round"
+  v-bind:disabled="page === pageTotal"
+  v-on:click="selectPage(page + 1)">
+    下一页
+
+  </button>
+  <button type="button" class="btn btn-default btn-white btn-round"
+   v-bind:disabled="page === pageTotal"
+  v-on:click="selectPage(pageTotal)">
+    {{pageTotal}}
+
+  </button>
+  <span class="m--padding-10">
+    每页
+    <select v-model="size">
+      <option value="1">1</option>
+       <option value="5">5</option>
+       <option value="10">10</option>
+       <option value="20">20</option>
+       <option value="50">50</option>
+       <option value="100">100</option>
+    </select>
+    条，共【{{total}}】条
+  </span>
+</div>
+</template>
+
+<script>
+export default {
+  name:'pagination',
+  props:{
+    list:{
+      type:Function,
+      default:null
+    },
+    itemCount:Number
+  },
+  data:function (){
+    return{
+      total:0,
+      size:10,
+      page:0,
+      pageTotal:0,
+      pages:[],
+    }
+  },
+  methods:{
+    render(page,total){
+      let _this=this;
+      _this.page=page;
+      _this.total=total;
+      _this.pageTotal=Math.ceil(total / _this.size);
+      _this.pages =  _this.getPageItems( _this.pageTotal,page, _this.itemCount || 5);
+    },
+    selectPage(page){
+      let  _this=this;
+      if(page < 1){
+        page = 1;
+      }
+      if (page > _this.pageTotal){
+        page = _this.pageTotal;
+      }
+      if (this.page !==page){
+        _this.page = page;
+        if (_this.list){
+          _this.list(page);
+        }
+      }
+    },
+    getPageItems(total,current,length){
+      let items =[];
+      if (length>=total){
+        for (let i =1; i <= total; i++){
+          items.push(i);
+        }
+      }else{
+        let base =0;
+        if (current - 0 >Math.floor((length - 1) / 2)){
+          base = Math.min(total,current - 0+Math.ceil((length - 1)/2))-length;
+        }
+        for (let i = 1;i <= length; i++){
+          items.push(base + 1);
+        }
+      }
+      return items;
+    }
+  }
+}
+</script>
+<style scoped>
+.pagination{
+  vertical-align: middle !important;
+  font-size:16px;
+  margin-top: 0;
+  margin-bottom: 10px;
+}
+.pagination button{
+  margin-right: 5px;
+}
+
+.btn-primary.active{
+  background-color: #2f7bba !important;
+  border-color: #27689d !important;
+  color: white !important;
+  font-weight: 600;
+}
+</style>
--- doc/db/all.sql
+++ doc/db/all.sql
@@ -6,9 +6,21 @@ create table `chapter`(
     primary key (`id`)
 ) engine=innodb default  charset=utf8 comment='大章';
 
-insert into `chapter`(id,course_id,name)values('00000000','00000000','测试大章');
-insert into `chapter`(id,course_id,name)values('00000001','00000000','测试大章2');
 
+insert into `chapter`(id,course_id,name)values('00000001','00000000','测试大章1');
+insert into `chapter`(id,course_id,name)values('00000002','00000000','测试大章2');
+insert into `chapter`(id,course_id,name)values('00000003','00000000','测试大章3');
+insert into `chapter`(id,course_id,name)values('00000004','00000000','测试大章4');
+insert into `chapter`(id,course_id,name)values('00000005','00000000','测试大章5');
+insert into `chapter`(id,course_id,name)values('00000006','00000000','测试大章6');
+insert into `chapter`(id,course_id,name)values('00000007','00000000','测试大章7');
+insert into `chapter`(id,course_id,name)values('00000008','00000000','测试大章8');
+insert into `chapter`(id,course_id,name)values('00000009','00000000','测试大章9');
+insert into `chapter`(id,course_id,name)values('00000010','00000000','测试大章10');
+insert into `chapter`(id,course_id,name)values('00000011','00000000','测试大章11');
+insert into `chapter`(id,course_id,name)values('00000012','00000000','测试大章12');
+insert into `chapter`(id,course_id,name)values('00000013','00000000','测试大章13');
+insert into `chapter`(id,course_id,name)values('00000014','00000000','测试大章14');
 
 drop table  if exists `test`;
 create table  `test` (
